539580cf153e2484b3fb82f81b59ea19166ac3da
Hossam <h.hassan@naqla.org>
2017-12-06 14:07:40 +0200

    add page index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<title>Olx Scrapper</title>
+<style>
+    body { text-align: center; padding: 150px; }
+    h1 { font-size: 50px; }
+    body { font: 20px Helvetica, sans-serif; color: #333; }
+    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+    a { color: #dc8100; text-decoration: none; }
+    a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>Olx Scrapper</h1>
+    <div>
+        <p>   <a href="OLX-scraper/scanpage.php?page=1&goNext=1&maxNext=10">Scan </a> , <a href="OLX-scraper/listpage.php">Browse </a> </p>
+
+    </div>
+</article>
